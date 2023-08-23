09939ee4a5e9630e5ca4ad2313b8b93278c8c20b
202120423 <202120423@TPCP04LAB1010>
2023-08-23 20:38:27 -0300

    Adicionar arquivos de projeto.

---
+++ trabavd/AumentoCalculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace trabavd
+{
+    public class AumentoCalculadora
+    {
+        public double CalcularAumento(double valorOriginal , double taxaAumento)
+        {
+            double aumento = valorOriginal * (taxaAumento / 100);
+            return aumento;
+        }
+        public double CalcularNovoValor(double valorOriginal,double aumento)
+        {
+            double novoValor = valorOriginal + aumento;
+            return novoValor;
+        }
+    }
+}
---
+++ trabavd/Program.cs
@@ -0,0 +1,22 @@
+﻿using trabavd;
+
+double valorOriginal = 100.0; //valor pronto.
+double taxaAumento = 10.0; //10% de aumento
+
+
+Console.WriteLine("Informe o valor Original do salário:");
+valorOriginal = double.Parse(Console.ReadLine());
+
+
+
+AumentoCalculadora calculadora = new AumentoCalculadora();
+
+double aumento = calculadora.CalcularAumento(valorOriginal, taxaAumento);
+double novoValor = calculadora.CalcularNovoValor(valorOriginal, aumento);
+
+
+Console.WriteLine($"Valor Original: R${valorOriginal:F2}");
+Console.WriteLine($"Taxa de Aumento: {taxaAumento}%");
+Console.WriteLine($"Novo valor: R${novoValor:F2}");
+
+//Trabalho Pedro e Samara
